c09001657c6911787ad108d48ef7d6dd97355d23
Jagadeesh Mahalingam <jagsisemailing@gmail.com>
2026-02-25 22:36:39 -0800

    Database
    Found and created a set up for initializing database

---
+++ backend/db.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base
+
+DB_URL = "sqlite:///./lostfound.sqlite3"
+
+engine = create_engine(DB_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
