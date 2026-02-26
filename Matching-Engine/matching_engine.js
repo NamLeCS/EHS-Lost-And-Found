d68e05f51f477247a72ad7ae0e75705a0a0a5b77
NamLeCS <nam.le94568@gmail.com>
2026-02-25 22:05:59 -0800

    Added fields dictionary
    These are the identification keywords that can be used for the items.

--- Matching-Engine/matching_engine.js
+++ Matching-Engine/matching_engine.js
@@ -1,1 +1,12 @@
-
+fields = {
+    "id": "string",
+    "name": "string",
+    "color": "string",
+    "location": "string",
+    "category": "string",
+    "description": "string",
+    "texture": "string",
+    "size": "string",
+    "material": "string",
+    "weight": "string",
+}
